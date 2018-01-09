0b7cf187d030caebeb5a7b932aeca2d1aba7bc87
Макар Копер <35015540+makarishche@users.noreply.github.com>
2018-01-09 18:29:22 +0200

    Ща все буит

--- main.py
+++ main.py
@@ -31,20 +31,17 @@ def handle_text(message):
 def start(message):
     bot.send_message(message.chat.id, 'Попросите у меня Вк, и я дам его вам, попросите Мамбу, и я тоже ее вам дам.\nНо не просите у меня кушать:)(все, кто попросит - покушают пиздюлей)')
 
-@bot.message_handler(commands=["dai_mambu"])
-def start(message):
-    bot.send_message(message.chat.id, "Держи {-Variable.MAMBA-}")
-
-
 
 @bot.message_handler(func=lambda message: message.text == "Получить мамбу МСК")
 def handle_text(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
     user_markup.row('1')
     user_markup.row('2')
     user_markup.row('3')
     user_markup.row('4')
     user_markup.row('5')
-    bot.send_message(message.chat.id, "Сколько?")
+    user_markup.row('/start')
+    bot.send_message(message.chat.id, "Сколько?", reply_markup=user_markup)
     @bot.message_handler(func=lambda message: message.text == "1")
     def command_text_hi(m):
           f = open('mamba.txt', 'r+')
@@ -53,9 +50,9 @@ def handle_text(message):
           print (a)
           f.close()
           mamba = a.pop (0)
+          del a[0]
           bot.send_message(m.chat.id, 'Держи: \n '+mamba)
           bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
-          del a[-1]
           print (mamba)
           print (a)
           f = open('mamba.txt', 'w')
@@ -67,19 +64,22 @@ def handle_text(message):
           a = s.split('\n')
           print (a)
           f.close()
-          @bot.message_handler(func=lambda message: message.text == "1")
-          def command_text_hi(m):
+    @bot.message_handler(func=lambda message: message.text == "2")
+    def command_text_hi(m):
           f = open('mamba.txt', 'r+')
           s = (f.read())
           a = s.split('\n')
           print (a)
           f.close()
           mamba = a.pop (0)
-          mamba1 = a.pop (1)
-          bot.send_message(m.chat.id, 'Держи: \n '+mamba '\n' +mamba2)
+          del a[0]
+          mamba1 = a.pop (0)
+          del a[0]
+          bot.send_message(m.chat.id, 'Держи: \n '+mamba)
+          bot.send_message(m.chat.id, mamba1)
           bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
-          del a[0,1,-1]
           print (mamba)
+          print (mamba1)
           print (a)
           f = open('mamba.txt', 'w')
           for index in a:
@@ -90,29 +90,166 @@ def handle_text(message):
           a = s.split('\n')
           print (a)
           f.close()
-    
-@bot.message_handler(func=lambda message: message.text == "Получить вк") 
-def command_text_hi(m):
-    f = open('vk.txt', 'r+')
-    s = (f.read())
-    a = s.split('\n')
-    print (a)
-    f.close()
-    vk = a.pop (0)
-    del a[-1]
-    bot.send_message(m.chat.id, 'Лови:) \n' +vk)
-    bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
-    print (vk)
-    print (a)
-    f = open('vk.txt', 'w')
-    for index in a:
-        f.write(index + '\n')
-    f.close
-    f = open('vk.txt', 'r+')
-    s = (f.read())
-    a = s.split('\n')
-    print (a)
-    f.close()
+    @bot.message_handler(func=lambda message: message.text == "3")
+    def command_text_hi(m):
+          f = open('mamba.txt', 'r+')
+          s = (f.read())
+          a = s.split('\n')
+          print (a)
+          f.close()
+          mamba = a.pop (0)
+          del a[0]
+          mamba1 = a.pop (0)
+          del a[0]
+          mamba2 = a.pop (0)
+          del a[0]
+          bot.send_message(m.chat.id, 'Держи: \n '+mamba)
+          bot.send_message(m.chat.id, mamba1)
+          bot.send_message(m.chat.id, mamba2)
+          bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+          print (mamba)
+          print (mamba1)
+          print (mamba2)
+          print (a)
+          f = open('mamba.txt', 'w')
+          for index in a:
+              f.write(index + '\n')
+          f.close
+          f = open('mamba.txt', 'r+')
+          s = (f.read())
+          a = s.split('\n')
+          print (a)
+          f.close()
+    @bot.message_handler(func=lambda message: message.text == "4")
+    def command_text_hi(m):
+          f = open('mamba.txt', 'r+')
+          s = (f.read())
+          a = s.split('\n')
+          print (a)
+          f.close()
+          mamba = a.pop (0)
+          del a[0]  
+          mamba1 = a.pop (0)
+          del a[0]
+          mamba2 = a.pop (0)
+          del a[0]
+          mamba3 = a.pop (0)
+          del a[0]
+          bot.send_message(m.chat.id, 'Держи: \n '+mamba)
+          bot.send_message(m.chat.id, mamba1)
+          bot.send_message(m.chat.id, mamba2)
+          bot.send_message(m.chat.id, mamba3)
+          bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+          print (mamba)
+          print (mamba1)
+          print (mamba2)
+          print (mamba3)
+          print (a)
+          f = open('mamba.txt', 'w')
+          for index in a:
+              f.write(index + '\n')
+          f.close
+          f = open('mamba.txt', 'r+')
+          s = (f.read())
+          A = s.split('\n')
+          print (a)
+          f.close()
+    @bot.message_handler(func=lambda message: message.text == "5")
+    def command_text_hi(m):
+          f = open('mamba.txt', 'r+')
+          s = (f.read())
+          a = s.split('\n')
+          print (a)
+          f.close()
+          mamba = a.pop (0)
+          del a[0]  
+          mamba1 = a.pop (0)
+          del a[0]
+          mamba2 = a.pop (0)
+          del a[0]
+          mamba3 = a.pop (0)
+          del a[0]
+          mamba4 = a.pop (0)
+          del a[0]
+          bot.send_message(m.chat.id, 'Держи: \n '+mamba)
+          bot.send_message(m.chat.id, mamba1)
+          bot.send_message(m.chat.id, mamba2)
+          bot.send_message(m.chat.id, mamba3)
+          bot.send_message(m.chat.id, mamba4)
+          bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+          print (mamba)
+          print (mamba1)
+          print (mamba2)
+          print (mamba3)
+          print (mamba4)
+          print (a)
+          f = open('mamba.txt', 'w')
+          for index in a:
+              f.write(index + '\n')
+          f.close
+          f = open('mamba.txt', 'r+')
+          s = (f.read())
+          A = s.split('\n')
+          print (a)
+          f.close()
+
+
+@bot.message_handler(func=lambda message: message.text == "Получить вк МСК") 
+def handle_text(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('1')
+    user_markup.row('2')
+    user_markup.row('3')
+    user_markup.row('4')
+    user_markup.row('5')
+    user_markup.row('/start')
+    bot.send_message(message.chat.id, "Сколько?", reply_markup=user_markup)
+    @bot.message_handler(func=lambda message: message.text == "1")
+    def command_text_hi(m):
+        vkmsk = open('vk.txt', 'r+')
+        svkmsk = (vkmsk.read())
+        vkmsk_list = svkmsk.split('\n')
+        print (vkmsk_list)
+        vkmsk.close()
+        vk = vkmsk_list.pop (0)
+        del vkmsk_list[0]
+        bot.send_message(m.chat.id, 'Лови:) \n' +vk)
+        bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+        print (vk)
+        print (vkmsk_list)
+        vkmsk = open('vk.txt', 'w')
+        for index in vkmsk_list:
+            vkmsk.write(index + '\n')
+        vkmsk.close
+        vkmsk = open('vk.txt', 'r+')
+        svkmsk = (vkmsk.read())
+        vkmsk_list = svkmsk.split('\n')
+        print (vkmsk_list)
+        vkmsk.close()
+    @bot.message_handler(func=lambda message: message.text == "2")
+    def command_text_hi(m):
+        f = open('vk.txt', 'r+')
+        s = (f.read())
+        a = s.split('\n')
+        print (a)
+        f.close()
+        vk = a.pop (0)
+        vk2 = a.pop (1)
+        bot.send_message(m.chat.id, 'Лови:) \n' +vk)
+        bot.send_message(m.chat.id, vk2)
+        bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+        print (vk)
+        print (vk2)
+        print (a)
+        f = open('vk.txt', 'w')
+        for index in a:
+            f.write(index + '\n')
+        f.close
+        f = open('vk.txt', 'r+')
+        s = (f.read())
+        a = s.split('\n')
+        print (a)
+        f.close()
 
 if __name__=="__main__":
     bot.polling()
