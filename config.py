8a2ad083ef0885c872ebd0c9fae7b66f16da6212
Макар Копер <35015540+makarishche@users.noreply.github.com>
2018-01-09 15:12:45 +0200

    Ну давай же

---
+++ config.py
@@ -0,0 +1,3 @@
+#coding:utf-8
+
+TOKEN = '478682565:AAG2dPlwCeg-mV9qYSOEEuRgbB1rTPK_jNQ'
---
+++ main.py
@@ -0,0 +1,85 @@
+#coding:utf-8
+import telebot, config
+from telebot import types
+import time
+import os
+
+knownUsers = []  # todo: save these in a file,
+userStep = {}  # so they won't reset every time the bot restarts
+
+commands = {  # command description used in the "help" command
+              'start': 'Get used to the bot',
+              'help': 'Gives you information about the available commands',
+              'sendLongText': 'A test using the \'send_chat_action\' command',
+              'getImage': 'A test using multi-stage messages, custom keyboard, and media sending'
+}
+
+
+bot=telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def handle_text(message): 
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('Получить мамбу') 
+    user_markup.row('Получить вк')
+    bot.send_message(message.from_user.id, 'Нажми на кнопку', reply_markup=user_markup)
+
+@bot.message_handler(commands=["help"]) 
+def start(message):
+    bot.send_message(message.chat.id, 'Попросите у меня Вк, и я дам его вам, попросите Мамбу, и я тоже ее вам дам.\nНо не просите у меня кушать:)(все, кто попросит - покушают пиздюлей)')
+
+@bot.message_handler(commands=["dai_mambu"])
+def start(message):
+    bot.send_message(message.chat.id, "Держи {-Variable.MAMBA-}")
+
+
+
+@bot.message_handler(func=lambda message: message.text == "Получить мамбу")
+def command_text_hi(m):
+    f = open('mamba.txt', 'r+')
+    s = (f.read())
+    a = s.split('\n')
+    print (a)
+    f.close()
+    mamba = a.pop (0)
+    bot.send_message(m.chat.id, 'Держи: \n '+mamba)
+    bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+    del a[-1]
+    print (mamba)
+    print (a)
+    f = open('mamba.txt', 'w')
+    for index in a:
+        f.write(index + '\n')
+    f.close
+    f = open('mamba.txt', 'r+')
+    s = (f.read())
+    a = s.split('\n')
+    print (a)
+    f.close()
+    
+@bot.message_handler(func=lambda message: message.text == "Получить вк") 
+def command_text_hi(m):
+    f = open('vk.txt', 'r+')
+    s = (f.read())
+    a = s.split('\n')
+    print (a)
+    f.close()
+    vk = a.pop (0)
+    del a[-1]
+    bot.send_message(m.chat.id, 'Лови:) \n' +vk)
+    bot.send_message(m.chat.id, 'Работай хорошо, тогда у тебя всегда будут свежие и красивые аккаунты;)')
+    print (vk)
+    print (a)
+    f = open('vk.txt', 'w')
+    for index in a:
+        f.write(index + '\n')
+    f.close
+    f = open('vk.txt', 'r+')
+    s = (f.read())
+    a = s.split('\n')
+    print (a)
+    f.close()
+
+if __name__=="__main__":
+    bot.polling()
